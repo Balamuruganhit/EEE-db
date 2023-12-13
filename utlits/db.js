33da63495833741e8615db57b0ec75635cfafc35
Balamuruganhit <balamuruganb011@gmail.com>
2023-12-13 11:25:24 +0530

    changes

--- utlits/db.js
+++ utlits/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql"
 
 const con=mysql.createConnection({
-	host:"localhost",
-	user:"root",
-	password:"",
-	database:"eeedb"
+	host:"blyj6jzurqqrwnonltok-mysql.services.clever-cloud.com",
+	user:"ujgdy4u68khtrhti",
+	password:"JpKDaTYkh4RUAyXxRjo2",
+	database:"blyj6jzurqqrwnonltok"
 })
 
 con.connect(function(err){
